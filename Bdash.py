57b2401fb004118932781e4f55b1b059ec08e0c7
MD Jafar <69416964+Jackweb199229@users.noreply.github.com>
2021-07-13 15:54:02 +0530

    Add files via upload

---
+++ Bdash.py
@@ -0,0 +1,14 @@
+from time import sleep
+def start():
+	name = "viewer"
+	print("\t\ti   i   i")
+	sleep(1)
+	print("\t\ti   i   i")
+	sleep(1)
+	print("\t:-------------------------------:")
+	print("\t:	Happy		+++++ : ")
+	sleep(1)
+	print("\t	+++++	Birthday ")
+	sleep(1)
+	print(f" \t:-    {name}	+++++	l	: ")
+		
